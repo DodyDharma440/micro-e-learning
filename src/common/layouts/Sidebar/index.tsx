ea82a16e89faf89f69961e728f6750076d37d535
Dodi Aditya <dodiaridharma@gmail.com>
2024-10-27 21:24:36 +0800

    fix: avatar profile

--- src/common/layouts/Sidebar/index.tsx
+++ src/common/layouts/Sidebar/index.tsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
 
           <Card className="p-4" isBlurred>
             <div className="flex items-center gap-3">
-              <Avatar name="John" />
+              <Avatar name={userData?.name} />
               <div>
                 <p className="mb-[2px] font-bold text-sm">{userData?.name}</p>
                 <p className="text-[10px] text-gray-400 dark:text-gray-500 capitalize">
