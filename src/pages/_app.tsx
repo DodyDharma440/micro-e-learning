42099e9f4e852888d4cfcd4c48392ad0460258a8
Dodi Aditya <dodiaridharma@gmail.com>
2024-11-25 00:18:21 +0800

    fix: disable react query retries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
       defaultOptions: {
         queries: {
           refetchOnWindowFocus: false,
+          retry: false,
         },
       },
     });
